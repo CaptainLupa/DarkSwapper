2221a10460d50e174470e79150d20c67a655dfc0
Jens Eckert <capt.lupa@gmail.com>
2022-09-04 01:22:32 -0400

    Trying more oauth stuff, not done yet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import "./index.css";
-import Body from "./components/Body";
-import React from "react";
+import React, { useCallback, useState } from "react";
+
+interface OauthProps {
+  authUrl: string;
+  clientId: number;
+  redirectUrl: string;
+}
+
+const useOauth2 = (props: OauthProps) => {
+  const { authUrl, clientId, redirectUrl } = props;
+
+  const [{ loading, error }, setUI] = useState({ loading: false, error: null });
+
+  const getAuth = useCallback(() => {
+    setUI({
+      loading: true,
+      error: null,
+    });
+  }, []);
+};
 
 class App extends React.Component {
   render() {
     return (
-      <div className=" flex flex-row justify-center items-center min-h-screen">
-        <Body />
-      </div>
+      <div className=" flex flex-row justify-center items-center min-h-screen"></div>
     );
   }
 }
